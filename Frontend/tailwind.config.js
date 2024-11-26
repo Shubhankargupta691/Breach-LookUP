24a545ec8ed62a94d4abdf53cf549bb0314fdc8c
Shubhankar691 <shubhankar691@gmail.com>
2024-11-26 17:13:58 +0530

    update: package

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,28 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       // Custom utilities
       scrollbarHide: {
-        '::-webkit-scrollbar': { display: 'none' },
-        '-ms-overflow-style': 'none',  // IE and Edge
-        'scrollbar-width': 'none',  // Firefox
+        "::-webkit-scrollbar": { display: "none" },
+        "-ms-overflow-style": "none", // IE and Edge
+        "scrollbar-width": "none", // Firefox
       },
     },
   },
   plugins: [
     function ({ addUtilities }) {
       addUtilities({
-        '.scrollbar-hide': {
-          '::-webkit-scrollbar': { display: 'none' },
-          '-ms-overflow-style': 'none',  // IE and Edge
-          'scrollbar-width': 'none',  // Firefox
+        ".scrollbar-hide": {
+          /* Hide scrollbar for Chrome, Safari, and Opera */ "-webkit-overflow-scrolling":
+            "touch",
+          "scrollbar-width": "none",
+          /* For Firefox */ "-ms-overflow-style":
+            "none" /* For Internet Explorer and Edge */,
+        },
+        ".scrollbar-hide::-webkit-scrollbar": {
+          display: "none" /* For Chrome, Safari, and Opera */,
         },
       });
     },
   ],
-}
+};
