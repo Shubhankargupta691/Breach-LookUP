89cc9c4563408d892739c3eb51f61e6a1b3bedc4
Shubhankar691 <shubhankar691@gmail.com>
2024-11-27 20:00:14 +0530

    refactor: remove unused functions and imports

--- Frontend/src/components/Scanner/components/Gauge.jsx
+++ Frontend/src/components/Scanner/components/Gauge.jsx
@@ -3,15 +3,14 @@ import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 import { colors } from '@mui/material';
 
 export function MaliciousGauge({ jsonData }) {
-  
+
   const stats = jsonData?.data?.attributes?.last_analysis_stats || {};
   const malicious = stats.malicious || 0;
   const suspicious = stats.suspicious || 0;
-  const timeout = stats.timeout || 0;
-  const failure = stats.failure || 0;
-  const typeunsupported = stats['type-unsupported'] || 0;
+  const undetected = stats.undetected || 0;
+  const harmless = stats.harmless || 0;
   const totalValue = malicious + suspicious;
-  const maxValue = totalValue + timeout + failure + typeunsupported;
+  const maxValue = totalValue + undetected + harmless;
 
   useEffect(() => {
 
