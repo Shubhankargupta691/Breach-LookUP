51150d2d2809978fa2468e5fcd0fbb24013745c8
Shubhankar691 <shubhankar691@gmail.com>
2024-12-10 02:38:59 +0530

    Style: Add shadow effect to enhance component appearance

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -9,6 +9,14 @@ export default {
         "-ms-overflow-style": "none", // IE and Edge
         "scrollbar-width": "none", // Firefox
       },
+      screens: {
+        xs: '600px',
+      },
+      boxShadow: {
+        'glow-blue': '0 0 10px 2px rgba(59, 130, 246, 0.7)', // Blue glow
+        'glow-red': '0 0 10px 2px rgba(220, 38, 38, 0.7)',   // Red glow
+        'glow-white': '0 0 10px 3px rgba(255, 255, 255, 0.7)', // White glow
+      },
     },
   },
   plugins: [
