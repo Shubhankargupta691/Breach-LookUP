8bd1e1a907fd5313eaeb5e0f106f0c3b0bff6e97
Shubhankar691 <shubhankar691@gmail.com>
2024-12-10 03:28:37 +0530

    Chore: Code Clean Up

--- Frontend/src/components/Scanner/components/script/ExtractFileData.jsx
+++ Frontend/src/components/Scanner/components/script/ExtractFileData.jsx
@@ -1,63 +1,67 @@
 import React from 'react';
-import {extractLastAnalysisStats, extractDistributors, extractTags, extractBasicInfo, extractTimeStamps } from '../../utils';
+import {extractAllData } from '../../utils';
 
-
-// Rename this File to FileCardDataExtractor
 
 export function getDetectionValue(jsonData) {
-  const stats = extractLastAnalysisStats(jsonData);
-  if (stats) {
-    const totalValue = (stats.malicious || 0) + (stats.suspicious || 0);
-    return totalValue;
-  }
-  return 0;
+  const { lastAnalysisStats } = extractAllData(jsonData);
+  const maliciousCount = lastAnalysisStats?.malicious || 0;
+  const suspiciousCount = lastAnalysisStats?.suspicious || 0;
+
+  return maliciousCount + suspiciousCount;
 }
 
 export function getDistributorsList(jsonData) {
-  const stats = extractDistributors(jsonData);
+  const {distributors} = extractAllData(jsonData);
   const detected = getDetectionValue(jsonData);
-  const message = `${detected > 0 
+  const message = `${ detected > 0 
     ? `${detected} security vendors flagged this file as malicious` 
     : 'No security vendors flagged this file as malicious'}`;
-  
-  return stats?.map(distributor => distributor) || [message];
+
+    if(!distributors || distributors.length === 0) {
+      return [message];
+    }
+
+  return distributors;
 }
 
 export function getTagsList(jsonData) {
-  const tags = extractTags(jsonData);
+  const tags = extractAllData(jsonData).tags;
   return tags.map(tag => tag) || [];
 }
 
 export function getLastSubmissionTime(jsonData) {
-  const timestamp = extractTimeStamps(jsonData);
-  return timestamp ? new Date(timestamp * 1000).toLocaleString() : 'N/A';
+  const timestamp = extractAllData(jsonData).lastAnalysisDate;
+  return timestamp ? new Date(timestamp * 1000).toUTCString() : 'N/A';
 }
 
 export function getFileDetails(jsonData, fileInfo) {
   const detectionValue = getDetectionValue(jsonData);
   const distributorsList = getDistributorsList(jsonData);
   const tagsList = getTagsList(jsonData);
-  const lastAnalysisDate = getLastSubmissionTime(jsonData);
+  const lastAnalysisDate =   getLastSubmissionTime(jsonData);
+  const uploadDate = fileInfo ? fileInfo?.lastModified : null;
+  const {nsrlInfo} =  extractAllData(jsonData);
   
-  const nameData = extractBasicInfo(jsonData);
+  const nameData = extractAllData(jsonData);
   let  hash256 = nameData.sha256;
-  let fileName = fileInfo?.name || nameData?.names[0] || nameData?.trusted_verdict?.filename || nameData?.known_distributors?.filenames[0] || 'no information';
-
-    if(!hash256){
-       return null;
+  
+    const fileName = fileInfo?.name || nameData.knownSources.filename || nameData.names[0] || nameData.distributors.filenames[0] ||   'no information';
+    
+    if (!hash256 || !fileName || fileName === '' || nsrlInfo === null || nsrlInfo === '') {
+      return null;
     }
-    if ((!fileName || fileName === '')) {
-       return null;
-    }
-
+  
   return {
     fileName: fileName,
-    fileSize: fileInfo ? (fileInfo.size / 1024).toFixed(2) + ' KB' : (nameData?.size/1048576).toFixed(2) + 'MB' || 'no information',
-    fileExt: fileInfo ? fileInfo.name.split('.').pop() : nameData?.type_extension || 'no information',
+    fileSize: fileInfo ? (fileInfo?.size / 1024).toFixed(2) + ' KB' : (nameData.size/1048576).toFixed(2) + 'MB' || 'no information',
+    fileExt: fileInfo ? fileInfo?.name.split('.').pop() : nameData.extension || null,
     fileHash: hash256,
-    lastAnalysisDate: lastAnalysisDate,
+    nsrlInfo : nsrlInfo,
+    lastAnalysisDate: uploadDate || lastAnalysisDate,
     detected: detectionValue,
     distributors: distributorsList,
     tags: tagsList
+
   };
 }
+
