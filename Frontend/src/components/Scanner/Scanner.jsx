6743b5491a9f1acda827863a427f4cac23c95171
Shubhankar691 <shubhankar691@gmail.com>
2024-12-10 02:02:49 +0530

    Fix: Removed unnecessary CSS

--- Frontend/src/components/Scanner/Scanner.jsx
+++ Frontend/src/components/Scanner/Scanner.jsx
@@ -3,10 +3,9 @@ import SearchBox from "./components/SearchBox";
 
 const Scanner = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-black text-white p-5">
-            <SearchBox />
-        </div>
-
+    <>
+        <SearchBox />
+    </>
     );
   
 }
