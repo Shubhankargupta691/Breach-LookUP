2685ac382af7ce4dbb029ccbd15ac188b7c65785
Shubhankar691 <shubhankar691@gmail.com>
2024-12-10 02:37:22 +0530

    Chore: Add index.js for centralized component exports

--- Frontend/src/components/Scanner/utils/index.js
+++ Frontend/src/components/Scanner/utils/index.js
@@ -1,6 +1,7 @@
 export * from './extractDataUtils';
 export * from './fileUtils';
 export * from './scanUtils';
+export {countries} from './countries';
 export { default as hashFile } from './fileUtils'
 export { default as inputTypes } from './inputTypes';
 export { default as Patterns } from './Patterns';
