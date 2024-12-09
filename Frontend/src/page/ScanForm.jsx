52de46138e027da972717f8543cf6b41133b974c
Shubhankar691 <shubhankar691@gmail.com>
2024-12-10 02:39:58 +0530

    Chore: Remove unused CSS styles

--- Frontend/src/page/ScanForm.jsx
+++ Frontend/src/page/ScanForm.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Scanner from '../components/Scanner/Scanner'
 const ScanForm = () => {
   return (
-    <div>
+    <>
       < Scanner />
-    </div>
+    </>
   )
 }
 
